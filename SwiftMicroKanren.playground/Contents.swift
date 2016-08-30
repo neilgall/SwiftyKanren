3dade2a7642588c23be98f9d397f0945eafdb04e
Neil Gall <neilgall@mac.com>
2016-08-30 22:48:59 +0100

    Tidy up infinite streams examples

--- SwiftMicroKanren.playground/Contents.swift
+++ SwiftMicroKanren.playground/Contents.swift
@@ -4,17 +4,23 @@
 let emptyState = State()
 
 func fives_(_ t: Term) -> Goal {
-    return disj(fresh{ $0 =~= .atom("5") }, callFresh{ _ in fives_(t) })
+    return disj(fresh{ $0 =~= .atom("5") }, fives)
 }
 let fives = callFresh(fives_)
 
 func sixes_(_ t: Term) -> Goal {
-    return disj(fresh{ $0 =~= .atom("6") }, callFresh{ _ in sixes_(t) })
+    return disj(fresh{ $0 =~= .atom("6") }, sixes)
 }
 let sixes = callFresh(sixes_)
 
 fives(emptyState).take(count: 5)
 disj(fives, sixes)(emptyState).take(count: 5)
+
+let aAndB: Goal =
+    callFresh { a in a =~= .atom("quark") }
+        && callFresh { b in (b =~= .atom("foo")) || (b =~= .atom("bar")) }
+
+aAndB(emptyState)
 
 let pair: Goal =
     fresh { w,x,y,z in
@@ -23,13 +29,5 @@ let pair: Goal =
         && (y =~= .pair(z, .atom("end")))
         && (z =~= .atom("bar"))
 }
-
-let aAndB: Goal =
-    callFresh { a in a =~= .atom("quark") }
-    && callFresh { b in (b =~= .atom("foo")) || (b =~= .atom("bar")) }
-
-let fooState = State(subs: [.variable(1): .atom("foo")], vars: 0)
-
 pair(emptyState)
-aAndB(emptyState)
-aAndB(fooState)
+
